63db572023991faa94d084fbfa91baab36336d18
gobanoff <a.kliosovas@yahoo.com>
2024-01-15 17:15:56 +0200

    color commit

--- src/App.js
+++ src/App.js
@@ -1,52 +1,44 @@
-import {  useEffect, useState } from "react";
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
   const [colorb, setColorb] = useState("");
   const [count, setCount] = useState(0);
 
- 
-
-
-const cl= () => {
-  setColorb('red'); setCount(count+1) ;
-}
-const cl1= () => {
-  setColorb('blue');setCount(count+1) ;
-}
-const cl2= () => {
-  setColorb('green');setCount(count+1) ;
-}
-const cl3= () => {
-  setColorb('yellow');setCount(count+1) ;
-}
-  return ( 
+  const cl = () => {
+    setColorb("red");
+    setCount(count + 1);
+  };
+  const cl1 = () => {
+    setColorb("blue");
+    setCount(count + 1);
+  };
+  const cl2 = () => {
+    setColorb("green");
+    setCount(count + 1);
+  };
+  const cl3 = () => {
+    setColorb("yellow");
+    setCount(count + 1);
+  };
+  return (
     <div className="App">
-      <div className='box'style={{ background: colorb }}><button className="btn btn-success"   onClick={cl}
-          >change</button>
-          <button className="btn btn-info"   onClick={cl1}
-          >change</button>
-          <button className="btn btn-warning"   onClick={cl2}
-          >change</button>
-          <button className="btn btn-primary"   onClick={cl3}
-          >change</button>
-          
-          <div className='box1'>{count}
-          
-          
-          
-          
-          </div>
-          
-          
-          </div>
+      <div className="box" style={{ background: colorb }}>
+        <button className="btn btn-success" onClick={cl}>
+          change
+        </button>
+        <button className="btn btn-info" onClick={cl1}>
+          change
+        </button>
+        <button className="btn btn-warning" onClick={cl2}>
+          change
+        </button>
+        <button className="btn btn-primary" onClick={cl3}>
+          change
+        </button>
 
-
-
-
-
-
-
+        <div className="box1">{count}</div>
+      </div>
     </div>
   );
 }
