b538f728c3d45e44dbaccd6c7847c43996a1be00
gobanoff <a.kliosovas@yahoo.com>
2024-01-15 15:18:01 +0200

    s

--- src/Calc.js
+++ src/Calc.js
@@ -7,141 +7,207 @@ function Calc() {
   const [action, setAction] = useState();
   const [sum, setSum] = useState();
 
-const result =(e)=>{
+  const result = (e) => {
     const act = {
-        '+': (a,b)=> a + b,
-        '-': (a,b)=> a - b,
-        '*': (a,b)=> a * b,
-        '/': (a,b)=> a / b
-       }
-        setSum (act[action](+first,+second));
-
-    }
-    const clear =(e)=>{
-        setFirst('');
-        setSecond('');
-        setAction();
-        setSum();
-        }
-
-
+      "+": (a, b) => a + b,
+      "-": (a, b) => a - b,
+      "*": (a, b) => a * b,
+      "/": (a, b) => a / b,
+    };
+    setSum(act[action](+first, +second));
+  };
+  const clear = (e) => {
+    setFirst("");
+    setSecond("");
+    setAction();
+    setSum();
+  };
 
   return (
     <>
+      <div className="calc">
+        <div className="numb">
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) setFirst(first + e.target.textContent);
+              else setSecond((value) => value + e.target.textContent);
+            }}
+          >
+            7
+          </button>
 
-<div className="calc">
-      <div className="numb">
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action) setFirst(first + e.target.textContent);
-            else setSecond((value) => value + e.target.textContent);
-          }}
-        >
-          7
-        </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) setFirst(first + e.target.textContent);
+              else setSecond((value) => value + e.target.textContent);
+            }}
+          >
+            8
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            9
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            4
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            5
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            6
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            1
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            2
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            3
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (!action) {
+                if (first === "0") return;
+                setFirst(first + e.target.textContent);
+              } else {
+                if (second === "0") return;
+                setSecond((value) => value + e.target.textContent);
+              }
+            }}
+          >
+            0
+          </button>
+          <button className="btn btn-danger" onClick={clear}>
+            {" "}
+            C{" "}
+          </button>
 
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action) setFirst(first + e.target.textContent);
-            else setSecond((value) => value + e.target.textContent);
-          }}
-        >
-          8
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first ==='0')return; setFirst(first + e.target.textContent);
-             } else{if (second ==='0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          9
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first ==='0')return; setFirst(first + e.target.textContent);}
-            else {if (second ==='0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          4
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first ==='0')return; setFirst(first + e.target.textContent);}
-            else{ if (second ==='0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          5
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first ==='0')return; setFirst(first + e.target.textContent);}
-            else{ if (second ==='0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          6
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first ==='0')return; setFirst(first + e.target.textContent);}
-            else{ if (second ==='0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          1
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first === '0')return; setFirst(first + e.target.textContent);}
-            else{ if (second === '0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          2
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first === '0')return; setFirst(first + e.target.textContent);}
-            else{ if (second === '0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          3
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={(e) => {
-            if (!action){if (first === '0')return; setFirst(first + e.target.textContent);}
-            else{ if (second === '0')return; setSecond((value) => value + e.target.textContent);}
-          }}
-        >
-          0
-        </button>
-        <button
-          className="btn btn-danger"
-          onClick={clear} > C </button>
-
-        <button
-          className="btn btn-danger"
-          onClick={result}>
-          =
-        </button>
-      </div>
-      <div className="actions">
-      <button className="btn btn-danger" onClick={(e) => {if (first!=="")setAction(e.target.textContent)}}>+</button>
-      <button className="btn btn-danger" onClick={(e) => {if (first!=="")setAction(e.target.textContent)}}>-</button>
-      <button className="btn btn-danger" onClick={(e) => {if (first!=="")setAction(e.target.textContent)}}>*</button>
-      <button className="btn btn-danger" onClick={(e) => {if (first!=="")setAction(e.target.textContent)}}>/</button>
-      </div>
+          <button className="btn btn-danger" onClick={result}>
+            =
+          </button>
+        </div>
+        <div className="actions">
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (first !== "") setAction(e.target.textContent);
+            }}
+          >
+            +
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (first !== "") setAction(e.target.textContent);
+            }}
+          >
+            -
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (first !== "") setAction(e.target.textContent);
+            }}
+          >
+            *
+          </button>
+          <button
+            className="btn btn-danger"
+            onClick={(e) => {
+              if (first !== "") setAction(e.target.textContent);
+            }}
+          >
+            /
+          </button>
+        </div>
       </div>
 
       <div className="rez">
-        {first}{action}{second}{sum}
+        {first}
+        {action}
+        {second}
+        {sum}
       </div>
     </>
   );
