2db11f215319d7af7d4f9041aeace57c904e87bf
gobanoff <a.kliosovas@yahoo.com>
2024-01-15 16:44:05 +0200

    xxxxx

--- src/App.js
+++ src/App.js
@@ -1,10 +1,43 @@
-
+import {  useState } from "react";
 import './App.css';
 
 function App() {
+  const [colorb, setColorb] = useState("");
+
+const cl= () => {
+  setColorb('red');
+}
+const cl1= () => {
+  setColorb('blue');
+}
+const cl2= () => {
+  setColorb('green');
+}
+const cl3= () => {
+  setColorb('yellow');
+}
   return ( 
     <div className="App">
-      <div className='box'><button className="btn btn-success">change</button></div>
+      <div className='box'style={{ background: colorb }}><button className="btn btn-success"   onClick={cl}
+          >change</button>
+          <button className="btn btn-info"   onClick={cl1}
+          >change</button>
+          <button className="btn btn-warning"   onClick={cl2}
+          >change</button>
+          <button className="btn btn-primary"   onClick={cl3}
+          >change</button>
+          
+          
+          
+          
+          </div>
+
+
+
+
+
+
+
     </div>
   );
 }
