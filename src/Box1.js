02aca6320f7dc6729e447a8cf8298a6ff3e40fd3
gobanoff <a.kliosovas@yahoo.com>
2024-01-20 02:34:08 +0200

    xo

--- src/Box1.js
+++ src/Box1.js
@@ -1,23 +1,76 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Box1.css";
 
 function Box1() {
+
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+
+  const calculateWinner = (squares) => {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        return squares[a];
+      }
+    }
+
+    return null;
+  };
+
+  const handleClick = (index) => {
+    const newBoard = [...board];
+
+    if (calculateWinner(newBoard) || newBoard[index]) {
+      return;
+    }
+
+    newBoard[index] = xIsNext ? "X" : "O";
+    setBoard(newBoard);
+    setXIsNext(!xIsNext);
+  };
+
+  const renderSquare = (index) => (
+    <button className="sqr" onClick={() => handleClick(index)}>
+      {board[index]}
+    </button>
+  );
+
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xIsNext ? "X" : "O"}`;
+
   return (
     <>
-      <div className="shot">
-        <div className="sqr">x</div>
-        <div className="sqr">x</div>
+      <div className="sht">Winner:{winner}</div>
 
-        <div className="sqr">x</div>
-        <div className="sqr">x</div>
-        <div className="sqr">x</div>
-        <div className="sqr">x</div>
-
-        <div className="sqr">x</div>
-        <div className="sqr">x</div>
-        <div className="sqr">x</div>
-
-
+      <div className="shot"> 
+     
+          {renderSquare(0)}
+          {renderSquare(1)}
+          {renderSquare(2)}
+          {renderSquare(3)}
+          {renderSquare(4)}
+          {renderSquare(5)}
+          {renderSquare(6)}
+          {renderSquare(7)}
+          {renderSquare(8)}
+      
       </div>
     </>
   );
