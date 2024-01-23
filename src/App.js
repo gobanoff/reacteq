af19f8ca7ebf8210d402dc483d5652fc728e28dd
gobanoff <a.kliosovas@yahoo.com>
2024-01-23 22:56:54 +0200

    aaaaa

--- src/App.js
+++ src/App.js
@@ -4,28 +4,33 @@ import "./App.css";
 const App = (props) => {
   const [colorb, setColorb] = useState("");
   const [count, setCount] = useState(0);
+  const [colorb1, setColorb1] = useState("");
 
   const cl = () => {
     setColorb("red");
     setCount(count + 1);
+    setColorb1("rgb(13, 230, 13)");
   };
   const cl1 = () => {
     setColorb("blue");
     setCount(count + 1);
+    setColorb1("red");
   };
   const cl2 = () => {
     setColorb("rgb(13, 230, 13)");
     setCount(count + 1);
+    setColorb1("yellow");
   };
   const cl3 = () => {
     setColorb("yellow");
     setCount(count + 1);
+    setColorb1("blue");
   };
 
   return (
     <div className="App">
       <div className="box" style={{ background: colorb }}>
-        <p>{count}</p>
+        <p style={{ color: colorb1 }}>{count}</p>
         <button className="btn btn-success" onClick={cl}>
           change
         </button>
